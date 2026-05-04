a63c18cc2ac7674e269a21e8497b0657e83fab3e
Zamzam Fahiye <zamzamft@MacBookPro.lan>
2026-05-04 15:57:25 +0200

    Initial outlining of folder structure

---
+++ MainProgram.cs
@@ -0,0 +1,4 @@
+//toplevel statement to skip default static 'Main' method boilorpoint
+using GMI24H_Lab1;
+
+Console.WriteLine("Hello, World!");
---
+++ PerformanceMeasurement.cs
@@ -0,0 +1,7 @@
+//toplevel statement to skip default static 'Main' method boilorpoint
+namespace GMI24H_Lab1;
+
+public static class PerformanceMeasurement
+{
+    
+}
